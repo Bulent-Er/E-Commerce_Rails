18da7b650d98363d9fa782c6a226717f8f4e3798
Bülent ER <bropil@gmail.com>
2023-10-01 23:25:14 +0300

    device and devise_token_auth gems were installed

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+
+  validates :first_namename, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :username, presence: true, length: { minimum: 2, maximum: 20 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+
   namespace :api do
     resources :products do
       get 'get_by_name', on: :collection
     end
-
     resources :categories do
     end
-
     root to: 'products#index'
     # Defines the root path route ("/")
     # root "articles#index"
+  end
+
+  scope :api do #If you use it with namespace, the files should be under that folder (Pay Atention!!!)
+    mount_devise_token_auth_for 'User', at: 'auth'
   end
 end
